7c357a4dd03196a07802b120588717b7e200badb
chengwei <chengwei9000@126.com>
2016-04-11 19:15:21 +0800

    commit new files

--- app/src/main/java/com/readnews/chengwei/model/ReadNewsModel.java
+++ app/src/main/java/com/readnews/chengwei/model/ReadNewsModel.java
@@ -1,8 +1,12 @@
 package com.readnews.chengwei.model;
 
+import com.readnews.chengwei.bean.News;
+
+import java.util.List;
+
 /**
  * Created by chengwei on 16/4/11.
  */
 public interface ReadNewsModel {
-
+    public List<News> getListFile();
 }
